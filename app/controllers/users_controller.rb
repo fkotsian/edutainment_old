2092912e6e66a4e28ef3d2f7a2276375c7a410cb
Frank Kotsianas <frankkotsianas@gmail.com>
2012-07-18 12:57:32 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user,	only: [:index, :edit, :update]
+  before_filter :correct_user, 		only: [:edit, :update]
+  before_filter :admin_user, 		only: :destroy
+  
+  def index 
+	@users = User.paginate(page: params[:page])
+  end	  
+  
   def new
 	@user = User.new
   end
   
   def show
 	@user = User.find(params[:id])
+  end
+  
+  def edit
   end
   
   def create
@@ -18,8 +29,37 @@ class UsersController < ApplicationController
 	end
   end
   
-  def index 
-    @users = User.all
-    render 'index'		# N.B. Not needed! :) Here to remember learning.
-  end	  
+  def update
+	if @user.update_attributes(params[:user])
+	  flash[:success] = "Your profile has been updated"
+	  sign_in @user			# We sign in as part of successful update because the remember token is reset when the user is saved; this allows a user to change login info and end any floating hijacked sessions
+	  redirect_to @user
+    else 
+	  render 'edit'
+	end
+  end
+  
+  def destroy
+	User.find(params[:id]).destroy
+	flash[:success] = "User destroyed."
+	redirect_to users_path
+  end
+  
+  private
+  
+    def signed_in_user
+	  unless signed_in?
+	    store_location
+		redirect_to signin_path, notice: "Please sign in." 
+	  end
+	end
+	
+	def correct_user
+	  @user = User.find(params[:id])
+	  redirect_to(root_path) unless current_user?(@user)
+	end
+	
+	def admin_user
+	  redirect_to root_path unless current_user.admin?
+	end
 end
