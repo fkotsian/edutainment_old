06b7439beff29cc2169bd405f0ff819711af6a08
Frank Kotsianas <frankkotsianas@gmail.com>
2012-08-05 20:12:22 -0700

    Fixing dependencies that prevent Heroku from working

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 class User < ActiveRecord::Base 
   attr_accessible :name, :title, :email, :password, :password_confirmation
   has_secure_password
-  has_many :microposts, dependent: :destroy
+  has_many :videos, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
