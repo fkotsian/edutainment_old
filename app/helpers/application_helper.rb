9f5964d7192bdf743a64cc93e7436f594439ef59
Frank Kotsianas <frankkotsianas@gmail.com>
2013-03-07 20:44:34 -0500

    Precompiling assets and changes to About Us page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the appropriate title (full or base) on a per-page basis
 	def full_title(page_title)
-		base_title = "EduTainment"
+		base_title = "EDUtainment"
 		if page_title.empty?
 			"Welcome to #{base_title}!"
 		else 
