0056269ff699b89a808eb291b3543441a02d0e33
Frank Kotsianas <frankkotsianas@gmail.com>
2013-03-14 06:55:47 -0400

    Add Mathman and Dr. MADD seeds to DB, populate with vids. Change _video display, and add (nonfunctional) video#edit page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = User.create[(name: "Lybroan James", 
+users = User.create[{name: "Lybroan James", 
 					 title: "For the Love of Math",
 					 nickname: "The Mathman",
 					 description: "Success is a function of your Order of Operations. Add good people, multiply time spent with them; divide the haters and subtract negative people. Solve for X. I love my X and you know Y :-).",
 					 email: "ldj004@mail.harvard.edu",
 					 password: "mathman",
-					 password_confirmation: "mathman")],
-				   [(name: "Mary Jo Madda",
+					 password_confirmation: "mathman"}],
+				   [{name: "Mary Jo Madda",
 					 title: "Dr. MADD's Lab",
 					 nickname: "Dr. MADD",
 					 description: " I'm a spicy meatball with a side of pastrami. You never know what I'm going to do next - JUST LIKE IN SCIENCE! Ahahahahaha. What was that? Whatever, back to the lab!",
 					 email: "mjm044@mail.harvard.edu",
 					 password: "drmadd"
-					 password_confirmation: "drmadd")]
+					 password_confirmation: "drmadd"}]
 					 
 Video.create(title: "The Mathman Begins",
 			 url: "http://vimeo.com/59434151",
